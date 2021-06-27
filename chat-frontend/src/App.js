d47f7d9a0e4ea5844752811816ae0060729cc6bb
Konstantin <mamonov.kosttya@gmail.com>
2021-06-27 18:43:14 +0500

    adding protected route

--- chat-frontend/src/App.js
+++ chat-frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import Chat from './components/Chat/Chat';
+import ProtectedRoute from './components/Router/ProtectedRoute';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './App.scss';
@@ -11,7 +12,7 @@ function App() {
     <Router>
         <div className="App">
           <Switch>
-            <Route exact path='/' component={Chat}/>
+            <ProtectedRoute Route exact path='/' component={Chat}/>
             <Route path='/login' component={Login}/>
             <Route path='/register' component={Register}/>
             <Route render={() => <h1>404 Страница не обнаружена</h1>} />
