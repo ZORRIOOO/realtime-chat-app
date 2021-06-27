6f578acc8864c00d3a67b9c3c790139fc7dfe933
Konstantin <mamonov.kosttya@gmail.com>
2021-06-27 12:37:23 +0500

    init compn. and add. react router

---
+++ chat-frontend/src/components/Auth/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+ 
+const Login = () => {
+    return (
+        <h1>Входная страница</h1>
+    );
+}
+
+export default Login
---
+++ chat-frontend/src/components/Chat/Chat.js
@@ -0,0 +1,9 @@
+import React from 'react';
+ 
+const Chat = () => {
+    return (
+        <h1>Страница с чатом</h1>
+    );
+}
+
+export default Chat
