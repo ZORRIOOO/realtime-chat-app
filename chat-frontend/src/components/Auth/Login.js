fa8e66f0a3b01230ae90a4f7db44efa071b01bad
Konstantin <mamonov.kosttya@gmail.com>
2021-06-27 13:06:17 +0500

    adding login component to chat

--- chat-frontend/src/components/Auth/Login.js
+++ chat-frontend/src/components/Auth/Login.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import loginImage from '../../assets/images/login.svg';
  
 const Login = () => {
     return (
-        <h1>Входная страница</h1>
+        <div id='auth-container'>
+            <div id='auth-card'>
+                <div>
+                    <div id='image-section'>
+                        <img src={loginImage} alt='Login' />
+                    </div>
+
+                    <div id='form-section'>
+                        <h2>Допро пожаловать</h2>
+
+                        <form>
+                            <div className='imput-field'>
+                                <input placeholder='Почта' />
+                            </div>  
+
+                            <div className='imput-field'>
+                                <input placeholder='Пароль' />
+                            </div>  
+
+                            <button>Войти</button>
+                        </form>
+
+                        <p>Ещё нет аккаунта? Регистрация</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
