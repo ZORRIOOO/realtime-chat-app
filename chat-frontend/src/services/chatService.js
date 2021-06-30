9f44287b7594abdd23f3e43fbeade3497e8a7b8a
Konstantin <mamonov.kosttya@gmail.com>
2021-06-30 17:17:20 +0500

    fetching chats and saving it in store

---
+++ chat-frontend/src/services/chatService.js
@@ -0,0 +1,17 @@
+import API from './api';
+
+const ChatService = {
+
+    fetchChats: () => {
+        return API.get('/chats')
+        .then(({data}) => {
+            return data
+        })
+        .catch(err => {
+            throw err
+        })
+    }
+
+}
+
+export default ChatService
