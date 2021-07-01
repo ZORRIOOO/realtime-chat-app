a4ac97d9c919fc7f4f2bd17e0a36c8b3f256066a
Konstantin <mamonov.kosttya@gmail.com>
2021-07-01 17:40:56 +0500

    adding socket.io to client side and testing it

--- chat-backend/socket/index.js
+++ chat-backend/socket/index.js
@@ -1,17 +1,20 @@
 const socketIo = require('socket.io');
 
 const SockerServer = (server) => {
-    const io = socketIo(server)
+    const io = require('socket.io')(server, {
+        cors: {
+            origin: '*',
+            transports: ['websocket']
+        }
+    });
 
     io.on('connection', (socket) => {
 
-        socket.on('join', async (user) => {
-
+    socket.on('join', async (user) => {     
             console.log('New user joined: ', user.firstName);
+
+            io.to(socket.id).emit('typing', 'Пользователь набирает...')
         })
-
-
-
     })
 
 }
