61ddff0c05aa89751c8be5f2e024f5742c84e693
Konstantin <mamonov.kosttya@gmail.com>
2021-07-01 19:30:51 +0500

    creating chatters query

--- chat-backend/socket/index.js
+++ chat-backend/socket/index.js
@@ -1,4 +1,5 @@
 const socketIo = require('socket.io');
+const { sequelize } = require('../models');
 
 const users = new Map()
 
@@ -28,7 +29,9 @@ const SockerServer = (server) => {
 
             const onlineFriends = [] // Массив id пользователей
 
-            const chatters = [] // Запрос пользователей, которые сейчас в сети
+            const chatters = await getChatters(user.id) // Запрос пользователей, которые сейчас в сети
+
+            console.log(chatters);
 
             // Уведомить друзей пользователя о том, что он сейчас в сети
             for (let i = 0; i < chatters.length; i++) {
@@ -52,6 +55,31 @@ const SockerServer = (server) => {
         })
     })
 
+}
+
+// Запрос к БД 
+
+const getChatters = async (userId) => {
+    try {
+        const [results, metadata] = await sequelize.query(`
+            SELECT "cu"."userId" FROM "ChatUsers" AS cu
+            INNER JOIN (
+                SELECT "c"."id" FROM "Chats" AS c
+                WHERE exists (
+                    SELECT "u"."id" FROM "Users" AS u
+                    INNER JOIN "ChatUsers" ON u.id = "ChatUsers"."userId"
+                    WHERE u.id = ${parseInt(userId)} AND c.id = "ChatUsers"."chatId"
+                )
+            ) AS cjoin ON cjoin.id = "cu"."chatId"
+            WHERE "cu"."userId" != ${parseInt(userId)}
+        `)
+
+        return results.length > 0 ? results.map(el => el.userId) : []
+
+    } catch (e) {
+        console.log(e);
+        return []
+    }
 }
 
 module.exports = SockerServer
