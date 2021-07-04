d10150c5e66615afdb61300f83a9fb1a1f4c9bbe
Konstantin <mamonov.kosttya@gmail.com>
2021-07-04 18:07:07 +0500

    creating add friend modal

--- chat-frontend/src/components/Chat/components/FriendList/FriendList.js
+++ chat-frontend/src/components/Chat/components/FriendList/FriendList.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Friend from '../Friend/Friend';
 import { setCurrentChat } from '../../../../store/actions/chat';
+import Modal from '../../../Modal/Modal';
 import './FriendList.scss';
 
 const FriendList = () => {
 
     const dispatch = useDispatch()
-
     const chats = useSelector(state => state.chatReducer.chats)
+
+    const [showFriendsModal, setShowFriendsModal] = useState(false)
+    const [suggestions, setSuggestions] = useState([])
 
     const openChat = (chat) => {
         dispatch(setCurrentChat(chat))
+    }
+
+    const searchFriends = (e) => {
+        // chat service
+    }
+
+    const addNewFriend = (id) => {
+        // dispatch 
     }
 
     return (
@@ -32,7 +43,33 @@ const FriendList = () => {
                     : <p id='no-chat'>У тебя нет друзей</p>
                 }
             </div>
+            {
+                showFriendsModal &&
+                <Modal>
+                    <Fragment key='header'>
+                        <h3 className='m-0'>Создать новый чат</h3>
+                    </Fragment>
 
+                    <Fragment key='body'>
+                        <p>Найти друзей:</p>
+                        <input 
+                            onInput={e => searchFriends(e)}
+                            type='text'
+                            placeholder='Найти'
+                        />
+                        <div id='suggestions'>
+                            {
+                                suggestions.map(user => {
+                                    return <div className='suggestion'>
+                                        <p className='m-0'>{user.firstName} {user.lastName}</p>
+                                        <button onClick={() => addNewFriend(user.id)}>Добавить</button>
+                                    </div>
+                                })
+                            }
+                        </div>
+                    </Fragment>
+                </Modal>
+            }
         </div>
     )
 }
